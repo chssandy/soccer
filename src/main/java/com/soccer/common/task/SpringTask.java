84c207465c56cd13dafafe52dd847a24c822a9f4
chs_sandy <chs_sandy@163.com>
2018-07-09 07:07:38 +0800

    定时任务

--- src/main/java/com/soccer/common/task/SpringTask.java
+++ src/main/java/com/soccer/common/task/SpringTask.java
@@ -32,42 +32,21 @@ public class SpringTask
 	
     private static LoggerAdapter log = LoggerAdapterFacory.getLoggerAdapter(SpringTask.class);
     
-    /**
-     * 每日凌晨三点 执行用户信用计算
-     * @throws JohnMediaException
-     */
-    @Scheduled(cron = "0 0 3 * * ?")
-    public void executeCreditCaculation() throws SysException
-    {
-        log.info("执行用户信用计算!");
-        //        log.info("处理打款超过48小时数据!");
-        //        balanceDao.doAlipayFailed();
-    }
-    
-    /**
-     * 每日凌晨2点 执行易发布状态修改
-     * @throws JohnMediaException
-     */
-	@Scheduled(cron = "0 0 0 * * ?")
-	public void fixYFBExecuteStatus() throws SysException {
-		log.info("执行易发布状态修改!");
-	}
-    
     
     /**
      * 每隔一分钟更新任务状态
      * @throws JohnMediaException
      */
-    @Scheduled(cron = "0 0/1 * * * ?")
+    @Scheduled(cron = "0 0/10 * * * ?")
     public void fixTaskStatus() throws SysException
     {
-//    	System.out.println("=========================================================================================");
-//    	LiveScoreSearch search = new LiveScoreSearch();
-//    	search.setPage(1);
-//    	search.setHavePage(1);
-//    	search.setRows(100);
-//    	search.setIs_resolved(0);
-//    	liveScoreService.washData(search);
+    	System.out.println("=========================================================================================");
+    	LiveScoreSearch search = new LiveScoreSearch();
+    	search.setPage(1);
+    	search.setHavePage(1);
+    	search.setRows(100);
+    	search.setIs_resolved(0);
+    	liveScoreService.washData(search);
     }
     
 }
